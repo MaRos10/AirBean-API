c0bdfffd7ab1e4fab4ced6d8d38f18b362a71be8
’Marina <148355147+MaRos10@users.noreply.github.com>
2024-06-10 13:46:58 +0200

    Updated adminAuth

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,19 +1,5 @@
 // Middleware to be used in routes to check if user is admin
 
-/* const adminAuth = (req, res, next) => {
-  if (req.user && req.user.role === "admin") {
-    // If user is logged in and admin
-    next();
-  } else {
-    // If user isn´t logged in or admin, send error-message
-    res.status(403).json({
-      success: false,
-      message: "You do not have the required permissions.",
-      status: 403,
-    });
-  }
-}; */
-
 const adminAuth = (req, res, next) => {
   if (global.currentUser && global.currentUser.role === "admin") {
     next();
